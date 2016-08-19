576360a59b765f9953313768383d10a4c97245b0
suhkha <suhkha92@gmail.com>
2016-08-19 14:42:17 -0500

    big and small bubble with click event

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -1,17 +1,29 @@
+
 //Get array of bubbles
 var bubbles = document.getElementsByClassName("bubble__song--hover");
-
+var song__cover;
+var bubble__song;
 //Iterate each bubbles
-for (var i = 0; i < bubbles.length; i++) {
-	bubbles[i].addEventListener("click", function(){
-		var song__cover;
-		var bubble__song;
+function bubbleEfect(){
+	for (var i = 0; i < bubbles.length; i++) {
+		
+		bubbles[i].addEventListener("click", function(){
 
-		bubble__song = this.classList.add("bubble__song--click");
+			song__cover = this.childNodes;
 
-		song__cover = this.childNodes;
-		song__cover[1].classList.add("song__cover--click");
+			if(event.target.className == "img-responsive song__cover song__cover--click"){
 
+				song__cover[1].classList.remove("song__cover--click");
+				bubble__song = this.classList.remove("bubble__song--click");
+			}else{
 
-	});
+				bubble__song = this.classList.add("bubble__song--click");
+				song__cover[1].classList.add("song__cover--click");
+			}
+
+		});
+
+	}
 }
+
+bubbleEfect();
