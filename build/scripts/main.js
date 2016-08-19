9e9703986c85f54711a228f08d25e406ab30bfe9
suhkha <suhkha92@gmail.com>
2016-08-19 15:19:03 -0500

    Show & hide lyrics

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -3,6 +3,7 @@
 var bubbles = document.getElementsByClassName("bubble__song--hover");
 var song__cover;
 var bubble__song;
+var song;
 //Iterate each bubbles
 function bubbleEfect(){
 	for (var i = 0; i < bubbles.length; i++) {
@@ -10,15 +11,21 @@ function bubbleEfect(){
 		bubbles[i].addEventListener("click", function(){
 
 			song__cover = this.childNodes;
+			song = this.nextElementSibling;
 
 			if(event.target.className == "img-responsive song__cover song__cover--click"){
 
-				song__cover[1].classList.remove("song__cover--click");
 				bubble__song = this.classList.remove("bubble__song--click");
+				song__cover[1].classList.remove("song__cover--click");
+				song.classList.add("hidden__song");
+				song.classList.remove("song");
+
 			}else{
 
 				bubble__song = this.classList.add("bubble__song--click");
 				song__cover[1].classList.add("song__cover--click");
+				song.classList.add("song");
+				song.classList.remove("hidden__song");
 			}
 
 		});
